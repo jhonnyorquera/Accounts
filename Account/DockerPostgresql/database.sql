5b7f5092491d605db1891efe76bdedf06fe7aa7f
Jhonny Orquera <49768684+jhonnyorquera@users.noreply.github.com>
2024-07-08 12:53:09 -0500

    Docker files

--- Account/DockerPostgresql/database.sql
+++
@@ -1,4 +0,0 @@
-drop table if exists account;
-drop table if exists customer;
-drop table if exists movement;
-drop table if exists person;
---
+++ Docker/DockerPostgresql/database.sql
@@ -0,0 +1,7 @@
+drop table if exists account;
+drop table if exists customer;
+drop table if exists movement;
+drop table if exists person;
+
+CREATE DATABASE account;
+CREATE DATABASE customer;
